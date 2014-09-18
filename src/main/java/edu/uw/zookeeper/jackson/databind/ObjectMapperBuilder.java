cc6abc3507070aa502a030497bb845ab6baead92
Lisa Glendenning <lisa@glendenning.net>
2014-09-18 09:53:34 -0700

    change ObjectMapper settings

--- src/main/java/edu/uw/zookeeper/jackson/databind/ObjectMapperBuilder.java
+++ src/main/java/edu/uw/zookeeper/jackson/databind/ObjectMapperBuilder.java
@@ -1,8 +1,10 @@
 package edu.uw.zookeeper.jackson.databind;
 
 import java.util.List;
+
 import com.fasterxml.jackson.databind.Module;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
 
 import edu.uw.zookeeper.common.Builder;
 
@@ -20,7 +22,7 @@ public abstract class ObjectMapperBuilder implements Builder<ObjectMapper> {
     }
     
     protected ObjectMapper getDefaultObjectMapper() {
-        return new ObjectMapper();
+        return new ObjectMapper().disable(SerializationFeature.FLUSH_AFTER_WRITE_VALUE).enable(SerializationFeature.WRITE_ENUMS_USING_INDEX);
     }
     
     protected abstract List<Module> getDefaultModules();
